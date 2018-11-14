2889022fb31e633af55a42972b315d387a32b3f0
root <root@localhost.localdomain>
2018-11-14 17:01:49 +0100

    created form and tested post

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+
+$first = $_POST['firstSite'];
+$second = $_POST['secondSite'];
+
+echo "First site is " . $first;
+echo "<br>";
+echo "Second site is " . $second;
+echo "<br>";
+
+?>
---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form method="POST" action="function.php">
+		<div>
+			<label>Site n.1</label>
+			<input type="text" name="firstSite" placeholder="Enter first site">
+		</div>
+		<div class="form-group">
+			<label>Site n.2</label>
+			<input type="text" name="secondSite" placeholder="Enter second site">
+		</div>
+		<button type="submit">Submit</button>
+</form>
+
+
+
+</body>
+</html>
